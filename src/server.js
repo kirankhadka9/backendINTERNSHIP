9e5f5ae36a7c26be2c75ea659d12ccb144ac97de
Kiran Khadka <kirankhadka@MACBOOK.local>
2026-02-13 15:19:38 +0545

    port added

--- src/server.js
+++ src/server.js
@@ -1,13 +1,17 @@
-require('dotenv').config();  // MUST be at the top
+require('dotenv').config(); 
 
 const connectDB = require('./connectToDb/connectDB.js');  
 const app = require('./app');
 
 const startServer = async () => {
   try {
-    await connectDB();  // wait for DB
+    await connectDB(); 
     const PORT = process.env.PORT || 3000;
-    app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+    
+  
+    app.listen(PORT, '0.0.0.0', () => {
+      console.log(`Server running on port ${PORT}`);
+    });
   } catch (err) {
     console.error('Server crashed:', err);
   }
